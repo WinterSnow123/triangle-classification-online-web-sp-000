00ff498159b9e3278d2158b83e5a19fd3ac73425
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-03 18:37:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,13 +10,8 @@ end
 
 def kind
   if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two < @side_three || @side_two + @side_three < @side_one || @side_one + @side_three < @side_two
-    begin
       raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
-  end
-  if @side_one == @side_two && @side_two == @side_three
+  elsif @side_one == @side_two && @side_two == @side_three
     :equilateral
   elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
     :isosceles
