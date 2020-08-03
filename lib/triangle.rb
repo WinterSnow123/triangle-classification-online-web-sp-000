2e4c08b561a7bb60549fd8ed980cdd5acede2b7f
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-03 18:32:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,17 @@ def initialize(side_one, side_two, side_three)
 end
 
 def kind
+  if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two < @side_three || @side_two + @side_three < @side_one || @side_one + @side_three < @side_two
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      error.message
+    end
   if @side_one == @side_two && @side_two == @side_three
     :equilateral
   elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
     :isosceles
-  elsif @side_one + @side_two > @side_three || @side_two + @side_three > @side_one || @side_one + @side_three > @side_two
+  else
     :scalene
   end
 end
