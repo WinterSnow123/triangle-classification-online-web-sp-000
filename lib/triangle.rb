cc68db6e127d2a648a911e8a0c3eaead319be01a
mirthful-concept-6030 <jchristinem@gmail.com>
2020-08-03 18:33:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,7 @@ def kind
     rescue TriangleError => error
       error.message
     end
+  end
   if @side_one == @side_two && @side_two == @side_three
     :equilateral
   elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
